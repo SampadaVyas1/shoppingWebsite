24b89f096652e97993bf2be580afd51a2ca83fb2
preetika <preetika.gautam@coditas.com>
2023-04-04 12:16:31 +0530

    created team page

--- hoc/layout/index.tsx
+++ hoc/layout/index.tsx
@@ -28,7 +28,7 @@ const Layout = ({ children }: any) => {
         <Splash />
       ) : (
         <div className={styles.layoutWrapper}>
-          <Navbar routes={role === 1 ? RECRUITER_ROUTES : ADMIN_ROUTES} />
+          <Navbar routes={role === 1 ? ADMIN_ROUTES : RECRUITER_ROUTES} />
           {router.pathname.includes("/team") ? (
             <React.Fragment>
               <TeamNavbar routes={TEAM_ROUTES} />
