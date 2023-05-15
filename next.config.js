d13bb2365b7af6f6778c451c8ec166b21291e2b8
nishu.kumari <nishu.kumari@coditas.com>
2023-05-15 14:29:05 +0530

    added exportPathMap in next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/login": { page: "/login" },
+      "/home": { page: "/" },
+      "/messages": { page: "/messages" },
+      "/candidates": { page: "/candidates" },
+      "/team": { page: "/team/recruiters" },
+    };
+  },
 };
 
 module.exports = nextConfig;
